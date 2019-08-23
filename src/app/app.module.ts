2a9c559b871d2931563292db3ada64beb1ccc39f
Blacknred <andyblacknred@gmail.com>
2019-08-23 19:02:13 +0300

    HomeComponent and RoomsGenerator (not finished)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { RoomsComponent } from './pages/rooms/rooms.component';
+import { ListComponent } from './components/list/list.component';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    RoomsComponent,
+    ListComponent,
+    PageHeaderComponent
   ],
   imports: [
     BrowserModule,
